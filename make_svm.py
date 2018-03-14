a23c6a6f6801b6763c21059556885101ce376c86
John-bardera <julia.bardera.jb@gmail.com>
2018-03-14 17:15:42 +0900

    add comment

--- make_svm.py
+++ make_svm.py
@@ -4,6 +4,7 @@ import os
 import numpy as np
 import cv2
 
+# HOGをとるためのパラメータ
 win_size = (60, 60)
 block_size = (16, 16)
 block_stride = (4, 4)
@@ -11,7 +12,7 @@ cell_size = (4, 4)
 bins = 9
 hog = cv2.HOGDescriptor(win_size, block_size, block_stride, cell_size, bins)
 
-
+# HOGを計算し、データとラベルのリストを返す関数
 def calc_hog():
 	train_and_labels = []
 	for file in os.listdir("./dataset/pos/"):
@@ -24,8 +25,9 @@ def calc_hog():
 		img = cv2.resize(img, win_size)
 		train_and_labels.append((hog.compute(img), 1))
 	
+	# 順番をランダムにする処理
+	# もっといい方法がありそう
 	np.random.shuffle(train_and_labels)
-	
 	train = []
 	label = []
 	for train_and_label in train_and_labels:
@@ -37,9 +39,13 @@ def calc_hog():
 if __name__ == "__main__":
 	train, label = calc_hog()
 
+	# SVMを新規作成
 	svm = cv2.ml.SVM_create()
+	# カーネル関数設定
 	svm.setKernel(cv2.ml.SVM_LINEAR)
+	# パラメータCを決定
 	svm.setType(cv2.ml.SVM_C_SVC)
 	svm.setC(0.5)
+	# 訓練
 	svm.train(train, cv2.ml.ROW_SAMPLE, label)
 	svm.save('train.xml')
--- try_svm.py
+++ try_svm.py
@@ -8,6 +8,7 @@ from selectivesearch import selective_search as ss
 
 start_time = time.time()
 
+# HOGをとるためのパラメータ
 win_size = (60, 60)
 block_size = (16, 16)
 block_stride = (4, 4)
@@ -15,22 +16,24 @@ cell_size = (4, 4)
 bins = 9
 hog = cv2.HOGDescriptor(win_size, block_size, block_stride, cell_size, bins)
 
+# 検出したい画像を選択
 target_path = "/mnt/c/Users/user/Pictures/dataset_favorite/"
-svm = cv2.ml.SVM_load("train.xml")
-
 target = os.listdir(target_path)[0]
 print(target)
 
+# 作成したSVMをロード
+svm = cv2.ml.SVM_load("train.xml")
+
+# HOG用とselectivesearch用に2種類用意
 img_bgr = cv2.imread(target_path + target)
 img = cv2.cvtColor(img_bgr.copy(), cv2.COLOR_BGR2GRAY)
-#resize_size = (int(img_bgr.shape[0]), int(img_bgr.shape[1]))
-#img = cv2.resize(img, resize_size)
-#img_bgr_resize = cv2.resize(img_bgr.copy(), resize_size)
-
 
 img_load_time = time.time()
 print(f"img_load:{img_load_time - start_time}")
 
+# selectivesearchを実行
+# img_lblには範囲で区切られている画像データが入っている
+# regionsはselectivesearchで得られるrectの情報とか
 img_lbl, regions = ss(img_bgr, scale=500, sigma=0.9, min_size=1000)
 
 ss_time = time.time()
@@ -40,8 +43,10 @@ hog_list = []
 hog_list_rect = []
 almosit_regions = len(regions)
 
+# HOGの結果をリストにまとめる
 for index, i in enumerate(regions):
 	x, y, w, h = i["rect"]
+	# rectの範囲が小さいものを排除
 	if h < win_size[1] or w < win_size[0]:
 		continue
 	print(f"hog: {index + 1}/{almosit_regions}")
@@ -49,6 +54,7 @@ for index, i in enumerate(regions):
 	hog_list.append(hog.compute(img_temp))
 	hog_list_rect.append((x, y, w, h))
 
+# SVMにHOGを渡し、検出する
 result = svm.predict(np.array(hog_list))[1].ravel()
 
 hog_and_svm_time = time.time()
@@ -56,10 +62,13 @@ print(f"hog_and_svm:{hog_and_svm_time - ss_time}")
 
 print(result)
 
+# 0、つまりは検出できたもののrectを保管
 recog_face_rect = [hog_list_rect[index] for index, i in enumerate(result) if i == 0.]
 
+# 赤色
 color = (0, 0, 255)
 
+# 元画像の検出できた先にrectを描画
 if recog_face_rect:
 	for i in recog_face_rect:
 		x, y, w, h = i
